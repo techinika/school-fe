a3c283ac02b6e4059b09df648df8f4a36add351a
songa1 <songachillethe1her@gmail.com>
2023-09-03 11:45:43 +0200

    Changed colors on cards

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,56 +1,54 @@
 import { CardGrid } from "@/components/Card/CardGrid";
-import { type } from "os";
+import img from "@/public/1.jpg";
 import React from "react";
 
 export default function Home() {
   const courses = [
     {
       id: 1,
-      image: "/1.jpg",
+      image: img,
       title: "JS for beginners",
-      description: "Learn JS from 0",
+      description:
+        "In this course, you will learn everything about JavaScript from beginner level and become a JavaScript expert in a matter of hours. Sit back and learn JavaScript.",
       author: "Achille",
-      price: "10,000",
+      price: "Subscription Required",
     },
     {
       id: 2,
-      image: "/1.jpg",
-      title: "JS for beginners",
-      description: "Learn JS from 0",
+      image: img,
+      title: "Learn Python for beginners",
+      description:
+        "Python is one of the most popular programming languages in the world and provides simple programming syntax for developers who are beginning their development journey.",
       author: "Achille",
       price: "10,000",
     },
     {
       id: 3,
-      image: "/1.jpg",
-      title: "JS for beginners",
-      description: "Learn JS from 0",
+      image: img,
+      title: "HTML & CSS Course",
+      description:
+        "Create a beautiful website designs with HTML and CSS. HTML helps you create a beautiful website framework and CSS helps you add beauty to it. For more information, learn this.",
       author: "Achille",
       price: "10,000",
     },
     {
       id: 4,
-      image: "/1.jpg",
-      title: "JS for beginners",
-      description: "Learn JS from 0",
+      image: img,
+      title: "REACT JS for beginners",
+      description:
+        "React is one of the most popular JavaScript libraries out there and offers simpplicity and many tools for developing modern web applications. Hop on, let's learn more about React.",
       author: "Achille",
-      price: "10,000",
-    },
-    {
-      id: 5,
-      image: "/1.jpg",
-      title: "JS for beginners",
-      description: "Learn JS from 0",
-      author: "Achille",
-      price: "10,000",
+      price: "Subscription Required",
     },
   ];
   return (
     <div>
-      <header className="bg-ucla-blue-hex text-white py-4">
+      <header className="py-4">
         <h1>Welcome to Techinika Technology School</h1>
       </header>
-      <CardGrid data={courses} type={"courses"} />
+      <div className="p-10">
+        <CardGrid data={courses} type={"courses"} />
+      </div>
       <footer className="bg-gradient-radial py-4 text-center text-white">
         &copy; 2023 Technika School
       </footer>
--- components/Card/Card.js
+++ components/Card/Card.js
@@ -4,20 +4,25 @@ export const Card = ({ card }) => {
   const { image, title, description, author, price } = card;
   return (
     <>
-      <div className="shadow-lg border-2 border-white rounded-xl">
+      <div className="shadow-lg rounded-xl text-white bg-outer-space-rgb">
         <div className="relative">
-          <Image src={image} width={300} height={200} />
+          <Image
+            src={image}
+            fill={true}
+            className="object-cover absolute"
+            alt={title}
+          />
         </div>
         <div className="p-5">
           <div>
             <h1 className="text-xl font-bold py-3">{title}</h1>
             <p>{description}</p>
           </div>
-          <div>
-            <div className="flex justify-between items-center">
-              <p>{author}</p>
-              <p>RWF {price}</p>
-            </div>
+          <div className="flex justify-between items-center py-2">
+            <p className="text-sm font-bold">{author}</p>
+            <p className="p-1 text-xs rounded-md border-gray border-2 bg-transparent">
+              {price}
+            </p>
           </div>
         </div>
       </div>
