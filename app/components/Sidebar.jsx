f1fd4fe3f0e5b06a77e166e500fb745327475d33
Furaha Emile <89402664+furahaemire@users.noreply.github.com>
2023-09-06 09:27:05 +0000

    some changes

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -6,20 +6,28 @@ function Sidebar() {
   return (
     <div>
 
-      
+
       <div class="topnav" id="myTopnav">
-      <div class="left">
-        <a href="" class="active"> Techinika</a>
+        <div class="left">
+          <a href="" class="active"> Techinika</a>
         </div>
         <div class="right">
-        <a href="">Home</a>
-        <a href="">All Course</a>
-        <a href="">Login</a>
-        <a href="">Sign up</a> 
-      </div>
-      </div>
+          <a href="">Home</a>
+          <a href="">All Course</a>
+          <a href="">Login</a>
+          <a href="">Sign up</a>
+        </div>
 
+        <div class="icon"
+        <i>
+          <div class="bar1"></div>
+          <div class="bar2"></div>
+          <div class="bar3"></div>
+        </i>
     </div>
+    </div>
+
+    </div >
 
   )
 }
