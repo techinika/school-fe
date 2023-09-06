41ca2fc31aa4c88c269efea5d0a8dd9657517f1a
furahaemire <furahaemire5@gmail.com>
2023-09-06 15:45:19 +0200

    some changes

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -12,15 +12,8 @@ function Sidebar() {
         <a href="#course">All Course</a>
         <a href="#login">Login</a>
         <a href="#signup">Sign Up</a>
-
-        <div class="icon">
-          <i>
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
-          </i >
-        </div>
       </div>
+      
     </div >
 
   )
