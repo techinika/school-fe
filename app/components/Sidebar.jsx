7ca8879e5735d1d2c2791ba135309254fe259cf4
furahaemire <furahaemire5@gmail.com>
2023-09-06 13:47:34 +0200

    changes

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -9,22 +9,25 @@ function Sidebar() {
 
       <div class="topnav" id="myTopnav">
         <div class="left">
-          <a href="" class="active"> Techinika</a>
+          <a href="" class="active"> Techinikaa</a>
         </div>
         <div class="right">
           <a href="">Home</a>
           <a href="">All Course</a>
           <a href="">Login</a>
           <a href="">Sign up</a>
-        </div>
+          </div>
 
         
-        <div class="icon"></div>
-        <i>
+        <div class="icon ">
+          <i>
           <div class="bar1"></div>
           <div class="bar2"></div>
           <div class="bar3"></div>
+          
         </i>
+    </div>
+    
     </div>
     </div>
 
